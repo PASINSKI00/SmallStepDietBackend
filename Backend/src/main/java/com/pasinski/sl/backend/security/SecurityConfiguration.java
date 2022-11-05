f39f20e8c3d5192caac392d67e20456d0daa5a04
PASINSKI00 <krystian.pasinski00@gmail.com>
2022-11-05 20:07:28 +0100

    Security config - permitAll

---
+++ Backend/src/main/java/com/pasinski/sl/backend/security/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.pasinski.sl.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .anyRequest().permitAll();
+        return http.build();
+    }
+}
