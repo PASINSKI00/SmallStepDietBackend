7c0bea068a1b89c6992bfe538e24f0ae8218ba10
PASINSKI00 <krystian.pasinski00@gmail.com>
2022-11-07 22:35:23 +0100

    disabled csrf

--- Backend/src/main/java/com/pasinski/sl/backend/security/SecurityConfiguration.java
+++ Backend/src/main/java/com/pasinski/sl/backend/security/SecurityConfiguration.java
@@ -14,7 +14,10 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .anyRequest().permitAll();
+                    .anyRequest().permitAll()
+                    .and()
+                    .csrf().disable();
+
         return http.build();
     }
 }
