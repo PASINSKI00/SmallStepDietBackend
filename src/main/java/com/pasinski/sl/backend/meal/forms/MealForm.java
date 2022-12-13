afca09b919732c773b6922fbb429c21ef893966a
PASINSKI00 <krystian.pasinski00@gmail.com>
2022-12-13 04:02:50 +0100

    deleted redundant import

--- src/main/java/com/pasinski/sl/backend/meal/forms/MealForm.java
+++ src/main/java/com/pasinski/sl/backend/meal/forms/MealForm.java
@@ -3,7 +3,6 @@ package com.pasinski.sl.backend.meal.forms;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Map;
