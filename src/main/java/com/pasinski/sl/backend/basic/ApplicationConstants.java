3ec68b4426b560b1a2e58a181da39115631bcc30
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-01-17 21:04:53 +0100

    constants for storing and retrieving user images

--- src/main/java/com/pasinski/sl/backend/basic/ApplicationConstants.java
+++ src/main/java/com/pasinski/sl/backend/basic/ApplicationConstants.java
@@ -3,12 +3,17 @@ package com.pasinski.sl.backend.basic;
 public class ApplicationConstants {
     public static final String DEFAULT_APP_URL = "http://localhost:8080";
     public static final String DEFAULT_MEAL_IMAGE_URL_WITH_PARAMETER = DEFAULT_APP_URL + "/api/image/meal?idMeal=";
-
+    public static final String DEFAULT_USER_IMAGE_URL_WITH_PARAMETER = DEFAULT_APP_URL + "/api/image/user?idUser=";
     public static final String DEFAULT_DIET_PDF_URL_WITH_PARAMETER = DEFAULT_APP_URL + "/api/diet/pdf?idDiet=";
-
     public static final String DEFAULT_GROCERIES_PDF_URL_WITH_PARAMETER = DEFAULT_APP_URL + "/api/diet/groceries/pdf?idDiet=";
+
+
     public static final String DEFAULT_MEAL_IMAGE_NAME = "default_meal.jpg";
+    public static final String DEFAULT_USER_IMAGE_NAME = "default_user.jpg";
+
+
     public static final String APP_DIRECTORY = "C:\\Users\\Krystian\\apps\\Dieter";
     public static final String PATH_TO_MEAL_IMAGES_DIRECTORY = APP_DIRECTORY + "\\images\\meals";
+    public static final String PATH_TO_USER_IMAGES_DIRECTORY = APP_DIRECTORY + "\\images\\users";
     public static final String PATH_TO_PDF_DIRECTORY = APP_DIRECTORY + "\\pdfs";
 }
