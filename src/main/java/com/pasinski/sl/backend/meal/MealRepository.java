e00bf4702bec4b60a6b015e5bd0b14ee9a9affeb
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-01-19 20:09:32 +0100

    added findAllByAuthor method

--- src/main/java/com/pasinski/sl/backend/meal/MealRepository.java
+++ src/main/java/com/pasinski/sl/backend/meal/MealRepository.java
@@ -1,6 +1,8 @@
 package com.pasinski.sl.backend.meal;
 
+import com.pasinski.sl.backend.user.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MealRepository extends JpaRepository<Meal, Long> {
+    Iterable<Meal> findAllByAuthor(AppUser appUser);
 }
