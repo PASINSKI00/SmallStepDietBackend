42ab52ec23bf32a2926ba667e20a0a5497201b49
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-01-24 04:39:29 +0100

    added constants related to posts images storing and retrieving

--- src/main/java/com/pasinski/sl/backend/basic/ApplicationConstants.java
+++ src/main/java/com/pasinski/sl/backend/basic/ApplicationConstants.java
@@ -4,6 +4,7 @@ public class ApplicationConstants {
     public static final String DEFAULT_APP_URL = "http://localhost:8080";
     public static final String DEFAULT_MEAL_IMAGE_URL_WITH_PARAMETER = DEFAULT_APP_URL + "/api/image/meal?idMeal=";
     public static final String DEFAULT_USER_IMAGE_URL_WITH_PARAMETER = DEFAULT_APP_URL + "/api/image/user?idUser=";
+    public static final String DEFAULT_POST_IMAGE_URL_WITH_PARAMETER = DEFAULT_APP_URL + "/api/image/post?idPost=";
     public static final String DEFAULT_DIET_PDF_URL_WITH_PARAMETER = DEFAULT_APP_URL + "/api/diet/pdf?idDiet=";
     public static final String DEFAULT_GROCERIES_PDF_URL_WITH_PARAMETER = DEFAULT_APP_URL + "/api/diet/groceries/pdf?idDiet=";
 
@@ -15,5 +16,6 @@ public class ApplicationConstants {
     public static final String APP_DIRECTORY = "C:\\Users\\Krystian\\apps\\Dieter";
     public static final String PATH_TO_MEAL_IMAGES_DIRECTORY = APP_DIRECTORY + "\\images\\meals";
     public static final String PATH_TO_USER_IMAGES_DIRECTORY = APP_DIRECTORY + "\\images\\users";
+    public static final String PATH_TO_POST_IMAGES_DIRECTORY = APP_DIRECTORY + "\\images\\posts";
     public static final String PATH_TO_PDF_DIRECTORY = APP_DIRECTORY + "\\pdfs";
 }
