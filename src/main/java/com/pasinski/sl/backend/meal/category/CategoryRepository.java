ea0a6d1db1389e00f31e0652e07a51a0844efb8d
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-07-29 20:51:45 +0200

    added findAllByNameIn method

--- src/main/java/com/pasinski/sl/backend/meal/category/CategoryRepository.java
+++ src/main/java/com/pasinski/sl/backend/meal/category/CategoryRepository.java
@@ -2,6 +2,9 @@ package com.pasinski.sl.backend.meal.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByName(String fast);
+    List<Category> findAllByNameIn(List<String> categories);
 }
