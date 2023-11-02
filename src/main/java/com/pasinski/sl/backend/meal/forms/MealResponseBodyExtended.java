171754439480f55c8b82f3354b2772a9fe06646e
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-11-02 11:46:45 +0100

    mealExtendedResponse extends mealResponse & refactor

--- src/main/java/com/pasinski/sl/backend/meal/forms/MealResponseBodyExtended.java
+++ src/main/java/com/pasinski/sl/backend/meal/forms/MealResponseBodyExtended.java
@@ -1,30 +1,29 @@
 package com.pasinski.sl.backend.meal.forms;
 
 import com.pasinski.sl.backend.file.S3Service;
+import com.pasinski.sl.backend.meal.Meal;
 import com.pasinski.sl.backend.meal.MealExtention;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
-@AllArgsConstructor
-public class MealResponseBodyExtended {
-    private String recipe;
-    private int timeToPrepare;
-    private int proteinRatio;
-    private int carbsRatio;
-    private int fatsRatio;
-    private List<ReviewResponseBody> reviews;
+public class MealResponseBodyExtended extends MealResponseBody {
+    private final String recipe;
+    private final int timeToPrepare;
+    private final int carbsRatio;
+    private final int fatsRatio;
+    private final List<ReviewResponseBody> reviews;
 
-    public MealResponseBodyExtended(MealExtention mealExtention, S3Service s3Service) {
-        this.recipe = mealExtention.getRecipe();
-        this.timeToPrepare = mealExtention.getTimeToPrepare();
-        this.proteinRatio = mealExtention.getProteinRatio();
-        this.carbsRatio = mealExtention.getCarbsRatio();
-        this.fatsRatio = mealExtention.getFatsRatio();
-        this.reviews = mealExtention.getReviews().stream()
+    public MealResponseBodyExtended(Meal meal, S3Service s3Service) {
+        super(meal, s3Service);
+        MealExtention mx = meal.getMealExtention();
+        this.recipe = mx.getRecipe();
+        this.timeToPrepare = mx.getTimeToPrepare();
+        this.carbsRatio = mx.getCarbsRatio();
+        this.fatsRatio = mx.getFatsRatio();
+        this.reviews = mx.getReviews().stream()
                 .map(review -> new ReviewResponseBody(review, s3Service)).collect(Collectors.toList());
     }
 }
