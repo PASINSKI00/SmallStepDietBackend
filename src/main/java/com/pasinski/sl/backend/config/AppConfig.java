75acfb4bd7062a532e8b294f1b4a582d34f6a683
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-11-23 19:31:19 +0100

    creating AppUserInterceptor bean

--- src/main/java/com/pasinski/sl/backend/config/AppConfig.java
+++ src/main/java/com/pasinski/sl/backend/config/AppConfig.java
@@ -1,6 +1,7 @@
 package com.pasinski.sl.backend.config;
 
 import com.pasinski.sl.backend.config.security.UserSecurityService;
+import com.pasinski.sl.backend.interceptors.AppUserInterceptor;
 import com.pasinski.sl.backend.user.AppUserRepository;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.context.annotation.Bean;
@@ -19,5 +20,10 @@ public class AppConfig {
     @Bean
     public ErrorAttributes errorAttributes() {
         return new CustomErrorAttributes();
+    }
+
+    @Bean
+    public AppUserInterceptor appUserInterceptor() {
+        return new AppUserInterceptor();
     }
 }
