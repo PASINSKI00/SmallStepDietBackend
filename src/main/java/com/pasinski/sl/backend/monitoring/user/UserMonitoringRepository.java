1dc01f0edfc971ce62c35eeda20adccff8dca332
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-11-24 18:56:54 +0100

    reimplemented userMonitoring

--- src/main/java/com/pasinski/sl/backend/monitoring/user/UserMonitoringRepository.java
+++ src/main/java/com/pasinski/sl/backend/monitoring/user/UserMonitoringRepository.java
@@ -1,7 +1,11 @@
 package com.pasinski.sl.backend.monitoring.user;
 
+import com.pasinski.sl.backend.monitoring.Action;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Timestamp;
 
 public interface UserMonitoringRepository extends JpaRepository<UserMonitoring, Long> {
+    Long countByActionEquals(Action action);
+    Long countByTimestampBetweenAndAction(Timestamp timestamp, Timestamp timestamp2, Action action);
 }
