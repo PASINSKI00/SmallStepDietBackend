cffd159e2a82fdc9a4ebac643d93e1ebab9f5fad
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-11-24 18:00:18 +0100

    added createdOn timestamp

--- src/main/java/com/pasinski/sl/backend/user/AppUser.java
+++ src/main/java/com/pasinski/sl/backend/user/AppUser.java
@@ -5,6 +5,7 @@ import com.pasinski.sl.backend.user.bodyinfo.BodyInfo;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -28,6 +29,10 @@ public class AppUser implements UserDetails {
     private boolean imageSet = false;
     private String imageName;
     private boolean isEmailVerified = false;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdOn;
 
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(
