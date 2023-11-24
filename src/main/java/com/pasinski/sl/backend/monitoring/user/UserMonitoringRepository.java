f6ad7448c53f878219413eb714db413272aba72a
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-11-24 18:30:02 +0100

    clean up usunsed imports

--- src/main/java/com/pasinski/sl/backend/monitoring/user/UserMonitoringRepository.java
+++ src/main/java/com/pasinski/sl/backend/monitoring/user/UserMonitoringRepository.java
@@ -2,9 +2,6 @@ package com.pasinski.sl.backend.monitoring.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.sql.Timestamp;
-import java.util.Collection;
 
 public interface UserMonitoringRepository extends JpaRepository<UserMonitoring, Long> {
-    Collection<Object> findAllByCreatedOnBetween(Timestamp createdOn, Timestamp createdOn2);
 }
