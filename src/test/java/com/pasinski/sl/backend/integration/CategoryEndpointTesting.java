bb9ae7df513bec7181efca60182b252b44bb18c9
PASINSKI00 <krystian.pasinski00@gmail.com>
2023-11-25 01:38:29 +0100

    initial tests for category endpoints

--- src/test/java/com/pasinski/sl/backend/integration/CategoryEndpointTesting.java
+++ src/test/java/com/pasinski/sl/backend/integration/CategoryEndpointTesting.java
@@ -15,5 +15,58 @@ public class CategoryEndpointTesting extends BaseForIntegrationTesting{
     String categoryEndpoint = "/api/category";
     String allCategoriesEndpoint = "/api/category/all";
 
+    @Test
+    @Order(1)
+    public void adminShouldBeAbleToPostCategory() throws IOException, InterruptedException {
+        //given
+        CategoryForm categoryForm = new CategoryForm("Category1");
+        String url = host + categoryEndpoint;
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/json")
+                .header("Authorization", adminUserAuthorizationHeader)
+                .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(categoryForm)))
+                .build();
 
+        //when
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+        //then
+        assertEquals(201, httpResponse.statusCode());
+    }
+
+    @Test
+    @Order(2)
+    public void getCategoriesEndpoint() throws IOException, InterruptedException {
+        //given
+        String url = host + allCategoriesEndpoint;
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        //when
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+        //then
+        assertEquals(200, httpResponse.statusCode());
+    }
+
+    @Test
+    public void userShouldNotBeAbleToPostCategory() throws IOException, InterruptedException {
+        //given
+        CategoryForm categoryForm = new CategoryForm("UnableToAdd");
+        String url = host + categoryEndpoint;
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .header("Content-Type", "application/json")
+                .header("Authorization", userAuthorizationHeader)
+                .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(categoryForm)))
+                .build();
+
+        //when
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+        //then
+        assertEquals(403, httpResponse.statusCode());
+    }
 }
